b864c477c322766a607f884c2d1f85aca9783fac
yaoxu <yaoxuu@qq.com>
2022-03-20 20:57:32 +0800

    init

---
+++ Flow/demo.js
@@ -0,0 +1,17 @@
+// @flow
+function sum(a: number, b: number) {
+  return a + b
+}
+
+function square(n) {
+  return n * n
+}
+
+square("1")
+
+const a: number = 1
+const b: string = "2"
+const c: boolean = false
+const d: null = null
+const e: void = undefined
+const f: symbol = Symbol()
